bb10e5666efaa33dd50255cf0326179409fc0f45
Qa-Automation30 <qautomation32@gmail.com>
2026-07-19 17:45:47 +0530

    Refactor TCs

--- tests/authentication/login.spec.ts
+++ tests/authentication/login.spec.ts
@@ -1,13 +1,14 @@
 import {expect, test} from '@playwright/test'
 import { LandingPage } from '../../src/pages/landingPage/LandingPage'
 import { LoginPage } from '../../src/pages/loginPage/LoginPage'
-import loginPageData from "../../src/testData/expectedData/loginPage.json";
+import loginExpectedData from "../../src/testData/expectedData/loginPage.json";
+import loginInputData from '../../src/testdata/inputTestData/loginPage.json'
 
 test("TC_002 - Verify login fails with invalid Email",async({page})=>{
         const landingPage = new LandingPage(page)
         const loginPage = new LoginPage(page)
         await landingPage.navigateToLandingPage()
         await landingPage.navigateToLoginPage()
-        await loginPage.submitLogin("TEST@gmail.com","Admin@123325")
-        expect(await loginPage.getWarnigMessage()).toBe(loginPageData.warningMessage)
+        await loginPage.submitLogin(loginInputData.invalidLoginDetails[0].emailAddress,loginInputData.invalidLoginDetails[0].password)
+        expect(await loginPage.getWarnigMessage()).toBe(loginExpectedData.warningMessage)
 })
